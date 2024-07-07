596cfb4298e656a975a988a8035e4f278d1057a1
vitorgsilvax <vgsilva@tractian.com>
2024-07-07 20:54:30 -0300

    feat(utils) add new utils

---
+++ src/utils/auth/bcryptjs/generateHashPassword/index.tsx
@@ -0,0 +1,11 @@
+import bcrypt from 'bcryptjs';
+
+export const generateHashPassword = async (password: string) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
+  } catch (error) {
+    console.log(error)
+  }
+}
