2fca839caa8a2410d2d10ee42ae0d6e8e3c7980e
murilo <murilorockstar492@gmail.com>
2024-07-11 21:39:06 -0300

    ajustado pro higor escrever

---
+++ src/app/(pages)/therms/page.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from 'next'
+import Footer from '@/components/common/Footer'
+
+const Page: NextPage = () => {
+  return (
+    <main>
+      <section>
+        <h1>Therms of Use </h1>
+        <p>escreve aqui higor</p>
+      </section>
+      <Footer />
+    </main>
+  )
+}
+
+export default Page
