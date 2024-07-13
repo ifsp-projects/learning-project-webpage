f05363e27b25efb7524eebdce338af70e3e2cb5c
Vitor Gabriel Silva - PESSOAL <vitorgabrielsbo1460@gmail.com>
2024-07-12 21:57:43 -0300

    feat(pages area-do-professor) added general view section

---
+++ src/app/(pages)/area-do-professor/components/GeneralView/data.tsx
@@ -0,0 +1,22 @@
+export const GENERAL_VIEW_FAKE_DATA = [
+  {
+    title: '10',
+    description: 'Tarefas para correção pendentes'
+  },
+  {
+    title: '85%',
+    description: 'de engajamento dos estudantes'
+  },
+  {
+    title: '4',
+    description: 'Disciplinas lecionadas atualmente'
+  },
+  {
+    title: '6',
+    description: 'Salas de Aula administradas'
+  },
+  {
+    title: '40',
+    description: 'Notificações pendentes'
+  }
+]
---
+++ src/app/(pages)/area-do-professor/components/GeneralView/types.ts
@@ -0,0 +1,8 @@
+export interface InsightProps {
+  title: string
+  description: string
+}
+
+export interface InsightCardProps {
+  insight: InsightProps
+}
