9a58d3059f9a45a36037ef99052bbdfae0287e15
Vitor Gabriel Silva - PESSOAL <vitorgabrielsbo1460@gmail.com>
2024-07-14 10:02:14 -0300

    feat(logged out homepage) added our services section

---
+++ src/app/(home)/NotLogged/components/OurServices/data.tsx
@@ -0,0 +1,36 @@
+import { BsGraphUp } from "react-icons/bs";
+import { FaRegLightbulb } from 'react-icons/fa'
+import { SiAwsorganizations } from 'react-icons/si'
+
+type ServicesIconsType = {
+  bulb: JSX.Element
+  dashboard: JSX.Element
+  report: JSX.Element
+  [key: string]: JSX.Element
+}
+
+export const SERVICES_DATA = [
+  {
+    title: 'Aprendizado',
+    description:
+      'Organize o aprendizado de seus estudantes de maneira dinâmica e eficaz com nossa plataforma.',
+    icon: 'bulb'
+  },
+  {
+    title: 'Análises',
+    description:
+      'Tenha um feedback detalhado com análise de dados e infográficos de suas aulas.',
+    icon: 'dashboard'
+  },
+  {
+    title: 'Organização',
+    description: "Organize suas aulas e matérias lecionadas em módulos, além de anexar materiais e conteúdos.",
+    icon: "report"
+  }
+]
+
+export const SERVICES_ICONS: ServicesIconsType = {
+  bulb: <FaRegLightbulb fill="#14b8a6" size={28} />,
+  dashboard: <BsGraphUp fill="#14b8a6" size={28} />,
+  report: <SiAwsorganizations fill="#14b8a6" size={28} />
+}
