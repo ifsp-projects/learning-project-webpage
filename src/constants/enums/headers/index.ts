983da58f1fd1e953e45d8c0f7f18ab653752b1ed
vitorgsilvax <vgsilva@tractian.com>
2024-07-21 09:39:56 -0300

    feat(improvements) improved and added new features

---
+++ src/constants/enums/headers/index.ts
@@ -0,0 +1,8 @@
+export enum HEADERS {
+  USER_AGENT = 'user-agent',
+  REFERRER = 'referer',
+  CLIENT_ID = 'x-client-id',
+  CLIENT_IP = 'x-client-ip',
+  PREVIEW_TOKEN = 'x-preview-token',
+  REQUEST_URI = 'x-request-uri'
+}
