2f9e360622e8705120f455b4a93c2283d7948f61
vitorgsilvax <vgsilva@tractian.com>
2024-07-28 16:23:01 -0300

    improvement(salas de aula) update activities section

--- src/app/(pages)/salas-de-aula/[slug]/components/Activities/index.tsx
+++ src/app/(pages)/salas-de-aula/[slug]/components/Activities/index.tsx
@@ -9,7 +9,7 @@ export const Activities: React.FC = () => {
     <Container as="section" data-cid="classroom-activities">
       <section className="flex w-full flex-col gap-8 rounded-sm border border-slate-300 bg-white px-4 py-8">
         <h2 className="text-lg font-semibold lg:text-xl">Atividades</h2>
-        <div className="flex w-full flex-col gap-4">
+        <div className="flex w-full flex-col">
           {ACTIVITIES.map((activity: Activity, index: number) => (
             <ActivityItem
               activity={activity}
