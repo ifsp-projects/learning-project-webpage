659f679efeee4dff9f22ffca4aae59a4b9fdfdf1
vitorgsilvax <vgsilva@tractian.com>
2024-08-09 19:48:05 -0300

    chore(next.config.mjs): implement eslint patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,28 @@
-import path from "path";
-import { fileURLToPath } from "url";
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')]
   },
   eslint: { ignoreDuringBuilds: true },
   reactStrictMode: false,
   images: {
     domains: [
-      "lh3.googleusercontent.com",
-      "res.cloudinary.com",
-      "img.clerk.com",
+      'lh3.googleusercontent.com',
+      'res.cloudinary.com',
+      'img.clerk.com'
     ],
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
-};
+        protocol: 'https',
+        hostname: '**'
+      }
+    ]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
