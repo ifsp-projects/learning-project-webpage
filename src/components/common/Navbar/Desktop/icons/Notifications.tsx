3613b7b441d7ffe5245aa226f8bd6a98c9d97555
Vitor Gabriel Silva - PESSOAL <vitorgabrielsbo1460@gmail.com>
2024-08-24 11:04:49 -0300

    feat(public icons): added new icons and updated navbar images

---
+++ src/components/common/Navbar/Desktop/icons/Notifications.tsx
@@ -0,0 +1,31 @@
+import { IconProps } from '@/types/components/icons'
+
+export const Notification: React.FC<IconProps> = props => {
+  return (
+    <svg
+      color="currentColor"
+      data-name="Layer 1"
+      height="24"
+      id="Layer_1"
+      strokeWidth="1.5"
+      viewBox="0 0 24 24"
+      width="24"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <defs>
+        <style>.cls-6374f8d9b67f094e4896c616-1</style>
+      </defs>
+      <path
+        className="cls-6374f8d9b67f094e4896c616-1"
+        d="M20.59,14.86V10.09A8.6,8.6,0,0,0,12,1.5h0a8.6,8.6,0,0,0-8.59,8.59v4.77L1.5,16.77v1.91h21V16.77Z"
+        fill="currentColor"
+      ></path>
+      <path
+        className="cls-6374f8d9b67f094e4896c616-1"
+        d="M14.69,18.68a2.55,2.55,0,0,1,.17,1,2.86,2.86,0,0,1-5.72,0,2.55,2.55,0,0,1,.17-1"
+        fill="currentColor"
+      ></path>
+    </svg>
+  )
+}
